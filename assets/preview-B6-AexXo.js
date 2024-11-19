b208880381b3505ded309fba083f24fb80927cab
LuLaValva <lukelavalva@gmail.com>
2024-11-19 15:07:37 -0800

    Updates

--- assets/preview-B6-AexXo.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-BNxP2yvO.js","./iframe-DH5MBqw2.js","./_commonjsHelpers-Cpj98o6Y.js","./_commonjs-dynamic-modules-TDtrdbi3.js","./index-pWxvJFOW.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as t}from"./iframe-DH5MBqw2.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-BNxP2yvO.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url);return new r}}};export{_ as parameters};
---
+++ assets/stories-BYICNWxC.js
@@ -0,0 +1,1 @@
+import{F as o}from"./from-file-7BpZzEpS.js";import"./_commonjsHelpers-Cpj98o6Y.js";const r={title:"From File",component:o},s={args:{}};export{s as Default,r as default};
